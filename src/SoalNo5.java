77e1d659ece4c0dfec92de21b1352e8ba81493f1
Maimito <64570623+Maimito@users.noreply.github.com>
2021-07-29 12:14:08 +0700

    revisi

--- src/SoalNo5.java
+++ src/SoalNo5.java
@@ -9,8 +9,7 @@ import java.util.*;
  * @author Lenovo
  */
 public class SoalNo5 {
-    static boolean palindrom(String kata){
-        String balik = new StringBuilder(kata).reverse().toString();
+    static boolean palindrom(String kata, String balik){
         if (kata.equalsIgnoreCase(balik)){
             return true;
         } else {
@@ -24,10 +23,10 @@ public class SoalNo5 {
         String kata = sc.nextLine();
         String balik = new StringBuilder(kata).reverse().toString();
         
-        if (palindrom(kata)){
-            System.out.println("True. "+balik+" adalah palindrom");
+        if (palindrom(kata, balik)){
+            System.out.println("True. "+kata+" adalah palindrom");
         } else {
-            System.out.println("False. "+balik+" bukan palindrom");
+            System.out.println("False. "+kata+" bukan palindrom");
         }
         
     }
